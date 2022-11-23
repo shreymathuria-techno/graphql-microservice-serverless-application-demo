5a32b8469037803d9fc2735adf1f2a00616b7d85
shreyjeavio <shreymathuria1989@gmail.com>
2022-11-23 23:30:44 +0530

    React serverless application demo
    Serverless application using React App, GraphQL and AWS (Amplify, AppSync, Lambda, DynamoDB)

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "graphqlmicroservice": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
